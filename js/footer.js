64d329abf21ba5a038e9e23e0e8f0bf54672d84e
joelCabana <joelxcabana@gmail.com>
2021-02-19 12:22:08 -0300

    footer y arreglos de estilos

---
+++ js/footer.js
@@ -0,0 +1,3 @@
+var copyright=new Date();
+update=copyright.getFullYear();
+var desarrolador = document.getElementById('desarrolador').innerHTML='&copy; Desarrollado por Joel Cabana - ' + update;
--- js/menu.js
+++ js/menu.js
@@ -5,8 +5,11 @@ btnMenu.addEventListener('click',function(){
     nav.classList.toggle('mostrar');
 })
 
-var btnOcultar = document.getElementById("btn-ocultar");
 
-btnOcultar.addEventListener('click',function(){
-    nav.classList.toggle('mostrar');
-})
+var items = document.getElementsByClassName('menu__link');
+
+for (var i=0; i< items.length; i++) {
+    items[i].addEventListener("click",function() {
+      nav.classList.toggle('mostrar');
+    });
+}
